1d34d41f9b335272b6a353a11ad53cf0333bc38f
DynamoRed <dynamored@hotmail.com>
2021-09-05 23:21:39 +0200

    Updating conf

--- conf.js
+++ conf.js
@@ -6,6 +6,6 @@ conf.general.prefix = '!';
 conf.general.guild = '884098680704077854';
 conf.general.id = '884116729905684530';
 
-conf.embeds = {};
+conf.embeds = {}; 
 conf.embeds.color = '5865F2';
 conf.embeds.footer = 'Maintained by DynamoRed#0001';
