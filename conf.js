ace9a8855f02d69032efe9deb62388dcd8059b34
DynamoRed <dynamored@hotmail.com>
2021-09-05 23:40:40 +0200

    Updating conf

--- conf.js
+++ conf.js
@@ -2,6 +2,7 @@ var conf = module.exports = {};
 
 conf.general = {};
 conf.general.name = 'Bot 2i2';
+conf.general.version = "beta-1.0.0";
 conf.general.prefix = '!';
 conf.general.guild = '884098680704077854';
 conf.general.id = '884116729905684530';
