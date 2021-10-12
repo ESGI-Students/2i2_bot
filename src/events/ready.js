b037caa72768f2a736f57e479f8a4015034dfa51
DynamoRed <raphael@dynamored.com>
2021-10-12 20:32:06 +0200

    Adding ready message

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,8 @@
 module.exports = {
 	event: 'ready',
 	once: true,
-	async execute() {
+	async execute(bot) {
 		console.log('Bot online !'.green);
+		bot.guilds.cache.get('884098680704077854').channels.cache.get('884103274704293899').send('Bot now online !')
 	},
 };
