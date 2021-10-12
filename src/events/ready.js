bbfbab22cf47ece2ba44d344e5a411ed31e1a845
DynamoRed <raphael@dynamored.com>
2021-10-12 20:38:48 +0200

    Fixing restart message

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
 	once: true,
 	async execute(bot) {
 		console.log('Bot online !'.green);
-		bot.guilds.cache.get('884098680704077854').channels.cache.get('884103274704293899').send('Bot now online !')
+		bot.channels.cache.get('884103274704293899').send('Bot now online !')
 	},
 };
