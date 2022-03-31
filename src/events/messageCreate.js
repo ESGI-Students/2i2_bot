737bf6d5c905384b71c55b6a1c72d8f0d5fb78b6
DynamoRed <raphael@dynamored.com>
2022-04-01 01:33:47 +0200

    Add april fool features

--- src/events/messageCreate.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'messageCreate',
-	async execute(message, bot) {
-        if(message.author.bot) return;
-        console.log("Message react!")
-        message.react("april_fool:959214780898500618")
-	}
-};
--- src/main.js
+++ src/main.js
@@ -4,18 +4,32 @@ const mysql = require('mysql');
 const conf = require('../conf');
 const { Client, Intents, Collection, MessageEmbed } = require('discord.js');
 const bot = new Client({
-	intents:[Intents.FLAGS.GUILDS],
+	intents: new Intents(32767),
 	presence: {
-		status: 'online',
-		activity: {
+		status: "dnd",
+		activities: [{
 			name: 'In Dev...',
 			type: 'PLAYING'
-		}
+		}]
 	}
 });
 const handlers = fs.readdirSync('./src/handlers').filter(file => file.endsWith('.js'));
 const events = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
 const commands = fs.readdirSync('./src/commands');
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const randomColor = () => {
+	let color = '#';
+	for (let i = 0; i < 6; i++){
+		const random = Math.random();
+		const bit = (random * 16) | 0;
+		color += (bit).toString(16);
+	};
+	return color;
+}
 
 bot.commands = new Collection();
 bot.games = [];
@@ -36,7 +50,7 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 bot.on("ready", () => {
-	bot.guilds.cache.get('884098680704077854').channels.cache.get('897847096881872977').send('Bot now online !')
+	bot.guilds.cache.get(conf.general.guild).channels.cache.get('897847096881872977').send('Bot now online !')
 });
 
 (async () => {
@@ -46,3 +60,30 @@ bot.on("ready", () => {
 	bot.handleCommands(commands, './src/commands');
 	bot.login(process.env.TOKEN);
 })();
+
+/**
+ * APRIL FOOL CODE
+ */
+
+bot.on("messageCreate", message => {
+	if(message.author.bot) return;
+	message.react("april_fool:959214780898500618")
+});
+
+setTimeout(() => {
+	const g = bot.guilds.cache.get(conf.general.guild);
+	g.members.cache.forEach(async m => {
+		if(m.roles.cache.find(r => r.name == "Classe")){
+			let role = g.roles.cache.find(r => r.name == m.id);
+			if(role){
+				setInterval(() => {
+					role.edit({color: randomColor()})
+				}, 5000)
+			} else {
+				console.log(`Create april fool role for ${m.user.tag}`.yellow.italic)
+				g.roles.create({name: m.id, color: randomColor(), reason: "April fool"})
+					.then(newR => m.roles.add(newR));
+			}
+		}
+	})
+}, 2500);
